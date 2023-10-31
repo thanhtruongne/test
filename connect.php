59e8f633dcb10c424d99bec2fa3cf35086cff28e
truong <nthanhtruong883@gmail.com>
2023-10-31 10:39:49 +0700

    thêm

--- connect.php
+++ connect.php
@@ -1,5 +1,5 @@
 <?php 
     $conn = mysqli_connect('localhost','root','','assign');
     if(!$conn) throw new ErrorException('', mysqli_connect_error());
- echo 123;echo 134134134;
+ echo 123;echo 134134134;echo 'ả';
 ?>
