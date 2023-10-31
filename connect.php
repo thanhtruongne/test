2b00d33c4ecbc9854f626caf13e03354e0ccdd9a
truong <nthanhtruong883@gmail.com>
2023-10-31 11:06:43 +0700

    addthem

--- connect.php
+++ connect.php
@@ -1,9 +1,5 @@
 <?php 
     $conn = mysqli_connect('localhost','root','','assign');
     if(!$conn) throw new ErrorException('', mysqli_connect_error());
-<<<<<<< HEAD
  echo 123;echo 134134134;echo 'ả';
-=======
- echo 123;echo 134134134;echo 'myfen';
->>>>>>> e31a7122108a474921f1e75277dea216fcc37e2f
 ?>
